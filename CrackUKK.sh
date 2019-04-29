7a10d06e5cd6d7a7472c1bca1b56e7cb36576a8d
FRLOfficial <50067432+FRLOfficial@users.noreply.github.com>
2019-04-29 16:00:56 +0700

    Add files via upload

---
+++ CrackUKK.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+#Lumayan Buat Pamer KeKwan Lu😂
+#Tools Ini Cuman Buat Iseng² doang
+#Dilarang Recode Sebelum Ijin FRL Official
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 3
+clear
+figlet "FRL_ID"|lolcat
+echo ""
+echo $cyan "Created by FRL Official"
+echo $cyan "Tools Crack Soal UKK"
+echo $cyan "WA : 085738995966"
+echo $cyan "FB : FRL_ID"
+echo $cyan "YT : FRL Official"
+echo $cyan "IG : @frl.23"
+echo $cyan "Notif : Belajarlah Dengan Giant"
+sleep 5
+echo $white "Waiting ........."
+sleep 10
+echo $cyan "Tools Crack Soal UK"
+echo $cyan "Berjumlah 12 Mata Pelajaran"
+echo $red "All Mahasiswa SMP. SMA. DAN SMK
+echo $green "Terdiri Dari : 
+echo $green "1)MTK"
+echo $green "2)B. INDO"
+echo $green "3)B. INGGRIS"
+echo $green "4)FISIKA"
+echo $green "5)B. JAWA"
+echo $green "6)KIMIA"
+echo $green "7)PKN"
+echo $green "8)IPS"
+echo $green "9)SENI BUDAYA"
+echo $green "10)TIK"
+echo $green "11)P. AGAMA"
+echo $green "12)PENJASKES"
+echo $green "0)EXIT"
+echo $purple "Info : Pilih Salah Satu Contoh 1 Jadi ketik Sesuai Mapel Yang Kamu Pilih"
+echo ""
+echo -e "╭─FRL@ROOT" |lolcat
+read -p "╰─#" pil;
+
+#MTK
+
+case $pil in
+1) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK{endc}:"
+echo $cyan "Tapi Boonk WKWKWKWKWK"
+echo
+
+
+;;
+
+
+#B.INDO
+
+
+2) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+
+;;
+
+
+#B. INGGRIS
+
+
+3) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+
+;;
+
+
+
+#FISIKA
+
+
+4) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+
+;;
+
+
+#B. JAWA
+
+
+5) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+
+;;
+
+
+#KIMIA
+
+
+6) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+;;
+
+
+#PKN
+
+
+7) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+;;
+
+
+#IPS
+
+
+8) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+;;
+
+
+#SENI BUDAYA
+
+
+9) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+;;
+
+
+#TIK
+
+
+10) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+;;
+
+
+#P. AGAMA
+
+
+11) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+;;
+
+
+#PENJASKES
+
+
+12) apt update && apt upgrade
+echo -e  "${y} {1} Masukan Nama Sesuai KK${endc}:"
+read KK
+echo $cyan "Tapi Boonk WKWKWKWK"
+echo
+
+;;
+
+
+13) echo "created by : FRL Official" | lolcat
+exit
+
+
+;;
+
+
+*) echo "sorry, pilihan yang anda cari tidak ada"
+esac
+done
+done
+
+
+echo "Terima Kasih ^_^"
+echo "Jangan Lupa Subscribe Chanel FRL Official"
